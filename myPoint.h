695e29fabe9a319601c549427464a60002668563
Ckson Liu <44485552+lty9520@users.noreply.github.com>
2018-10-26 19:51:09 +0800

    has changed origin master

---
+++ myPoint.h
@@ -0,0 +1,14 @@
+#ifndef MYPOINT_H
+#define MYPOINT_H
+
+class mpoint{                       //class point(x, y)
+public:
+	double x;
+	double y;
+	mpoint(double xx = 0, double yy = 0){
+		x = xx;
+		y = yy;
+	}
+
+};
+#endif	//MYPOINT_H
